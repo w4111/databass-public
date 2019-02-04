3990a5828023c725879811e69cdad9e1101245db
ewu <ewu@cs.columbia.edu>
2019-02-04 12:49:15 -0500

    lazy commit msg

--- submit.py
+++ submit.py
@@ -2,29 +2,19 @@ import click
 import subprocess
 import shutil
 
-assignments = ['aa%s' % i for i in range(1,6)]
+assignments = ['aa%s' % i for i in range(0,4)]
 digits = "1234567890"
 check_bad_uni = lambda uni: uni is None or any(c not in digits for c in uni[-4:])
-
-
-def validate_uni(ctx, param, uni):
-  if check_bad_uni(uni):
-    if param == "-u2" and uni == 'NONE': 
-      return
-    msg = "UNI should be in the format of AA1234.\nYou submitted: %s" % uni
-    raise click.BadParameter(msg)
 
 @click.command()
 @click.option("-u1", 
   prompt="Your UNI in the format of AA1234", 
-  help="UNI in AA1234 format",
-  callback=validate_uni)
-@click.option("-u2", 
-  prompt="Your teammate's UNI or the word 'NONE' if you don't have a teammate", 
-  help="Teammate's UNI, or 'NONE' if no teammate",
-  callback=validate_uni)
+  help="UNI in AA1234 format")
+@click.option("-u2",
+  prompt="Your teammate's UNI or the word 'NONE' if you don't have a teammate",
+  help="Teammate's UNI, or 'NONE' if no teammate")
 @click.option('-a', prompt="The assignment you are submitting", type=click.Choice(assignments))
-@click.option('-q', is_flag=True, help="Submit without prompting")
+@click.option('-q', is_flag=True, help="Create ZIP file without prompting")
 def main(u1, u2, a, q):
   """
   Script to package up your DataBass submission.  You should run this in a UNIX-based environment.
@@ -44,7 +34,7 @@ def main(u1, u2, a, q):
     else:
       print("Your teammate's UNI should be in the format of AA1234 or 'NONE' if you are working alone.")
       print("You submitted: %s" % uni2)
-      return 
+      return
 
   if assignment == None:
     print("Choose an assignment.  Use --help to see options")
@@ -57,7 +47,8 @@ def main(u1, u2, a, q):
 
   # Package and check the code
   fname = "%s_%s_%s" % (assignment, uni1, uni2)
-  shutil.make_archive(fname, 'zip', "src", ".")
+  shutil.make_archive(fname, 'zip', "databass", ".")
+  print "Created %s.zip  MAKE SURE TO SUBMIT THE ZIP FiLE!" % fname
 
 
 if __name__ == "__main__":
